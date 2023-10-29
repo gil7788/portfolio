600f3822a09d486943b1015d2a3fde83700fb643
gil7788 <gil7788@gmail.com>
2023-10-29 17:24:56 +0200

    Fix Project Cards Grid

--- src/components/sections/projects.js
+++ src/components/sections/projects.js
@@ -19,13 +19,13 @@ const StyledProjectsSection = styled.section`
   .projects-grid {
     ${({ theme }) => theme.mixins.resetList};
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-    grid-gap: 15px;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 1rem;
     position: relative;
     margin-top: 50px;
 
     @media (max-width: 1080px) {
-      grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+      grid-template-columns: 1fr;
     }
   }
 
